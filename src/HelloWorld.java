680e75aa0a1c17dcf666d5c3e0cfcbd9c56e02e7
mai05410 <mai.1027hachi@i.softbank.jp>
2019-04-19 16:58:15 +0900

    文字列の追加

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello,World!");
+		System.out.println("Hello,World!");
+		System.out.println("やっぱり帰るね！おやすみ！");
 
 	}
 
